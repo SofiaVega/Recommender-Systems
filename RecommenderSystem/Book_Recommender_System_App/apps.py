17b32af53d16babf0bc37d2d3021602afdf7ba3e
Javier Urrecha <javiergerardo7@gmail.com>
2022-01-06 17:36:54 -0700

    Added data.csv and web app features

---
+++ RecommenderSystem/Book_Recommender_System_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BookrecommendersystemappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Book_Recommender_System_App'
